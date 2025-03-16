906030caef2985fcb97f44514a680bd17577e630
Lzsaam <samuel.lima@sptech.school>
2025-03-16 17:42:04 -0300

    Adicionando VER SENHA

--- public/js/login-cadastro.js
+++ public/js/login-cadastro.js
@@ -194,3 +194,20 @@ document.getElementById('cpf').addEventListener('input', function (e) {
     value = value.replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4"); 
     e.target.value = value;
   });
+
+
+  // Ver senha
+  document.getElementById("togglePassword").addEventListener("click", function () {
+    let passwordInput = document.getElementById("password_signup");
+    let icon = this;
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+});
